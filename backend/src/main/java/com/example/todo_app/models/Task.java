540f3a7dec051d98642e4755f2a1bddc77d84354
tuteredurie <tuteredurie@hotmail.com>
2024-06-02 13:13:50 +1200

    init

---
+++ backend/src/main/java/com/example/todo_app/models/Task.java
@@ -0,0 +1,22 @@
+package com.example.todo_app.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity(name = "task")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String content;
+    private Boolean done;
+}
